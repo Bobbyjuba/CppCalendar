b240019415b440deda7cad472b738c3d178a4bd3
Brian Roden <32974706+Bobbyjuba@users.noreply.github.com>
2019-03-07 16:16:26 -0600

    Added a Basic Menu
    No real functionality with this commit. Just getting started.

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <ctime>
+#include <iostream>
+
+void main() {
+	int menuOption;
+
+	std::cout << "1. View My Events" << std::endl;
+	std::cout << "2. Add An Event" << std::endl;
+	std::cout << "3. Remove an Event" << std::endl << std::endl;
+	
+	std::cout << "What would you like to do? ";
+	std::cin >> menuOption;
+}
